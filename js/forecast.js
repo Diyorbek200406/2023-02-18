d32a0f5466401345ebce1070c9964ba503b41225
Erkinov Diyorbek <erkinovdiyorbek39@gmail.com>
2023-02-18 20:28:12 +0500

    salom hammaga diyorbekdan

--- js/forecast.js
+++ js/forecast.js
@@ -5,9 +5,9 @@ const KEY = "dcdd8a5a9fde98bf810ed3b884c0e992";
 const getData = async (city) => {
   const base = "https://api.openweathermap.org/data/2.5/weather";
   const query = `?q=${city}&units=metric&appid=${KEY}`;
-  loader(true)
+  loader(true);
   const req = await fetch(base + query);
   const data = await req.json();
-  loader(false)
+  loader(false);
   return data;
 };
